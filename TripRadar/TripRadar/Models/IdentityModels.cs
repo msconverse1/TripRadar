448a440b7e50c86a8ef03ccf9a2b70660943de59
Kanwarjit Rupyal <kanwar.rupyal@gmail.com>
2019-02-13 09:43:48 -0600

    Stagin to pull user

--- TripRadar/TripRadar/Models/IdentityModels.cs
+++ TripRadar/TripRadar/Models/IdentityModels.cs
@@ -20,7 +20,7 @@ namespace TripRadar.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        DbSet<Trip> Trip { get; set; }
+        public DbSet<Trip> Trip { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
@@ -32,9 +32,6 @@ namespace TripRadar.Models
             return new ApplicationDbContext();
         }
 
-<<<<<<< HEAD
-        public System.Data.Entity.DbSet<TripRadar.Models.Trip> Trips { get; set; }
-=======
->>>>>>> b63abc371e0523229dda26f424c4ab272e16b95f
+
     }
 }

defd49424a4f40bea40d0853be552967bbbbad76
msconverse1 <msconverse@fullsail.edu>
2019-02-13 08:44:19 -0600

    Create Weather Table
    Beinging to retrive weather infomation based off of a location entered
    by the user

--- TripRadar/TripRadar/Models/Weather.cs
+++ TripRadar/TripRadar/Models/Weather.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,22 @@ namespace TripRadar.Models
 {
     public class Weather
     {
+
+        [Key]
+        public int WeatherId { get; set; }
+        [Display(Name = "Tempature")]
+        public float mainTemp { get; set; }
+        [Display(Name = "Wind Speed")]
+        public float speedvalue { get; set; }
+        [Display(Name = "Wind Name")]
+        public string windName { get; set; }
+        [Display(Name = "Could Cover")]
+        public float cloudValue { get; set; }
+        [Display(Name = "Cloud Name")]
+        public string cloudName { get; set; }
+        [Display(Name = "Precipitation Value")]
+        public float precipitationValue { get; set; }
+        [Display(Name = "Precipitation Name")]
+        public string precipitationName { get; set; }
     }
 }
